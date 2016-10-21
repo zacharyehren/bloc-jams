500a5e0cd3850647c891b9a7f26342632526975d
Zachary Ehren <zachehren@gmail.com>
2016-10-20 18:43:37 -0700

    complete checkpoint 17 collection and album views

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate = 
+//#1
+var buildCollectionItemTemplate = function(){
+    var template =
     '<div class="collection-album-container column fourth">'
 + '  <img src="assets/images/album_covers/01.png"/>'
 + '  <div class="collection-album-info caption">'
@@ -13,15 +15,21 @@ var collectionItemTemplate =
 + '  </div>'
 + '</div>'
 ;
+    
+    //#2
+    return $(template);
+};
 
-window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
-
+$(window).load(function() {
     // #3
+    var $collectionContainer = $('.album-covers');
+    // #4
+    $collectionContainer.empty();
+
+    
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        //#5
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
